5efbb6fde2a29e820f2529fc4513c1065b2c641a
pcheh <konstantein.lebedev@mail.ru>
2024-10-01 02:57:27 +0500

    task 1

---
+++ TimeStruct/TimeStruct.UnitTests/TimeTests.cs
@@ -0,0 +1,147 @@
+namespace TimeStruct.UnitTests
+{
+    [TestFixture]
+    public class TimeTests
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var time = new Time(7, 44, 30);
+
+            Assert.That(time.Hours, Is.EqualTo(7));
+            Assert.That(time.Minutes, Is.EqualTo(44));
+            Assert.That(time.Seconds, Is.EqualTo(30));
+        }
+
+        [TestCase(-2)]
+        public void HoursSet_NegativeValue_ArgumentException(int val)
+        {
+            var time = new Time();
+
+            Assert.That(() => time.Hours = val, Throws.ArgumentException);
+        }
+
+        [TestCase(-2)]
+        [TestCase(80)]
+        public void MinutesSet_NegativeOrBigValue_ArgumentException(int val)
+        {
+            var time = new Time();
+
+            Assert.That(() => time.Minutes = val, Throws.ArgumentException);
+        }
+
+        [TestCase(-2)]
+        [TestCase(80)]
+        public void SecondsSet_NegativeOrBigValue_ArgumentException(int val)
+        {
+            var time = new Time();
+
+            Assert.That(() => time.Seconds = val, Throws.ArgumentException);
+        }
+
+        [TestCase(2, 12, 32, 7952)]
+        [TestCase(0, 0, 0, 0)]
+        public void ValueInSecondsTest(
+            int hours, int minutes, int seconds, int result)
+        {
+            var time = new Time(hours, minutes, seconds);
+
+            Assert.That(time.DurationInSeconds, Is.EqualTo(result));
+        }
+
+        [TestCase(2, 12, 32, "2:12:32")]
+        [TestCase(0, 0, 0, "0:0:0")]
+        public void ToStringTest(int hours, int minutes, int seconds, string result)
+        {
+            var time = new Time(hours, minutes, seconds);
+
+            Assert.That(time.ToString(), Is.EqualTo(result));
+        }
+
+        [TestCase(60, 60, true)]
+        [TestCase(60, 61, false)]
+        public void Equals_TwoTimes_ExpectedResult(
+            int hours1, int hours2, bool result)
+        {
+            var time1 = new Time(hours1, 0, 0);
+            var time2 = new Time(hours2, 0, 0);
+
+            Assert.That(time1.Equals(time2), Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Equals_WrongArgument_ArgumentException()
+        {
+            var time = new Time();
+            var smth = new object();
+
+            Assert.That(() => time.Equals(smth), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void GetHashCodeTest()
+        {
+            var x = new Time(5, 6, 7);
+            var y = new Time(5, 6, 7);
+            var z = new Time(5, 5, 5);
+
+            Assert.That(x.Equals(y), Is.True);
+            Assert.That(x.Equals(z), Is.False);
+        }
+
+        [Test]
+        public static void ComparisonTest()
+        {
+            var x = new Time(30, 10, 15);
+            var y = new Time(30, 10, 15);
+            var z = new Time(30, 55, 11);
+
+            Assert.That(x == y, Is.True);
+            Assert.That(x != y, Is.False);
+            Assert.That(x == z, Is.False);
+            Assert.That(x != z, Is.True);
+
+            Assert.That(x >= y, Is.True);
+            Assert.That(x >= z, Is.False);
+            Assert.That(z >= x, Is.True);
+
+            Assert.That(z > x, Is.True);
+            Assert.That(x > z, Is.False);
+            Assert.That(x > y, Is.False);
+
+            Assert.That(x <= y, Is.True);
+            Assert.That(x <= z, Is.True);
+            Assert.That(z <= x, Is.False);
+
+            Assert.That(x < z, Is.True);
+            Assert.That(z < x, Is.False);
+            Assert.That(x < y, Is.False);
+        }
+
+        [TestCase(30, 10, 15, 10, 55, 15, 41, 5, 30)]
+        [TestCase(30, 10, 15, 0, 0, 0, 30, 10, 15)]
+        public void AdditionTest(
+            int hours1, int minutes1, int seconds1,
+            int hours2, int minutes2, int seconds2,
+            int resultHours, int resultMinutes, int resultSeconds)
+        {
+            var time1 = new Time(hours1, minutes1, seconds1);
+            var time2 = new Time(hours2, minutes2, seconds2);
+            var result = new Time(resultHours, resultMinutes, resultSeconds);
+
+            Assert.That(time1 + time2, Is.EqualTo(result));
+        }
+
+        [TestCase(5, 11, 33, 45, 57, 48, 45)]
+        [TestCase(0.5, 11, 33, 45, 5, 46, 52)]
+        public void MultiplicationTest(double k,
+            int hours, int minutes, int seconds,
+            int resultHours, int resultMinutes, int resultSeconds)
+        {
+            var time = new Time(hours, minutes, seconds);
+            var result = new Time(resultHours, resultMinutes, resultSeconds);
+
+            Assert.That(k * time, Is.EqualTo(result));
+        }
+    }
+}
---
+++ TimeStruct/TimeStruct/Time.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace TimeStruct
+{
+    public struct Time
+    {
+        const int maxValue = 59;
+        const int secondsInMinute = 60;
+        const int secondsInHour = 3600;
+
+        int hours;
+
+        public int Hours
+        {
+            get => hours;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Значение должно быть неотрицательным");
+
+                hours = value;
+            }
+        }
+
+
+        int minutes;
+
+        public int Minutes
+        {
+            get => minutes;
+            set
+            {
+                if (value < 0 || value > maxValue)
+                    throw new ArgumentException("Значение должно быть неотрицательным и не более 59");
+
+                minutes = value;
+            }
+        }
+
+        int seconds;
+        public int Seconds
+        {
+            get => seconds;
+            set
+            {
+                if (value < 0 || value > maxValue)
+                    throw new ArgumentException("Значение должно быть неотрицательным и не более 59");
+
+                seconds = value;
+            }
+        }
+
+        public int DurationInSeconds
+        {
+            get => Hours * secondsInHour + Minutes * secondsInMinute + Seconds;
+        }
+
+        public Time(int hours, int minutes, int seconds) : this()
+        {
+            Hours = hours;
+            Minutes = minutes; 
+            Seconds = seconds;
+        }
+
+        public override string ToString() => $"{Hours}:{Minutes}:{Seconds}";
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Time)
+                return DurationInSeconds == ((Time)obj).DurationInSeconds;
+
+            throw new ArgumentException("Объект не является временным интервалом");
+        }
+
+        public override int GetHashCode() => DurationInSeconds.GetHashCode();
+
+        public static bool operator ==(Time x, Time y) => x.Equals(y);
+        public static bool operator !=(Time x, Time y) => !x.Equals(y);
+        public static bool operator >(Time x, Time y) => x.DurationInSeconds > y.DurationInSeconds;
+        public static bool operator <(Time x, Time y) => x.DurationInSeconds < y.DurationInSeconds;
+        public static bool operator >=(Time x, Time y) => (x.DurationInSeconds >= y.DurationInSeconds);
+        public static bool operator <=(Time x, Time y) => (x.DurationInSeconds <= y.DurationInSeconds);
+
+        public static Time operator +(Time x, Time y) =>
+            GetTimeByDurationInSeconds(x.DurationInSeconds + y.DurationInSeconds);
+
+        public static Time operator -(Time x, Time y)
+        {
+            if (x > y)
+                return GetTimeByDurationInSeconds(x.DurationInSeconds + y.DurationInSeconds);
+                
+            throw new ArgumentException("Вычитаемое должно быть меньше уменьшаемого");
+        }
+
+        public static Time operator *(double k, Time time) =>
+            GetTimeByDurationInSeconds((int)Math.Round(k * time.DurationInSeconds));
+
+        public static Time operator *(Time time, double k) => k * time;
+
+
+        private static Time GetTimeByDurationInSeconds(int val)
+        {
+            int seconds = val;
+            int hours = seconds / secondsInHour;
+            seconds %= secondsInHour;
+            int minutes = seconds / secondsInMinute;
+            seconds %= secondsInMinute;
+
+            return new Time(hours, minutes, seconds);
+        }
+    }
+}
