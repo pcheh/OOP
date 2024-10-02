0afe82eaeb5e3eb38ffcbcb2c43f7163e167ef38
pcheh <konstantein.lebedev@mail.ru>
2024-10-02 14:32:30 +0500

    added Contrast and Interlace Fix filters

---
+++ Task3-genericFilter/PhotoEnhancer/PhotoEnhancer/Filters/ContrastParameters.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class ContrastParameters : IParameters
+    {
+        public double Coefficient { get; set; }
+        public ParameterInfo[] GetDecription()
+        {
+            return new[]
+            {
+                new ParameterInfo()
+                {
+                    Name = "Коэффициент",
+                    MinValue = 0,
+                    MaxValue = 10,
+                    DefaultValue = 1,
+                    Increment = 0.05
+                }
+            };
+        }
+
+        public void SetValues(double[] values)
+        {
+            Coefficient = values[0];
+        }
+
+        public double AdjustContrast(double intensity)
+        {
+            double newIntensity = Coefficient * (intensity - 0.5) + 0.5;
+
+            if (newIntensity < 0)
+                newIntensity = 0;
+
+            if (newIntensity > 1)
+                newIntensity = 1;
+
+            return newIntensity;
+        }
+    }
+}
