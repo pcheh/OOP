c6d10705766b9157cd1b002f72b3c3d1edb02088
pcheh <konstantein.lebedev@mail.ru>
2024-10-06 15:00:27 +0500

    1, 2, 3 done

---
+++ exam-task/Report/Report/ReportMakers/IStatisticsCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Report
+{
+    public interface IStatisticsCalculator
+    {
+        string Caption { get; }
+
+        object MakeStatistics(IEnumerable<double> data);
+    }
+}
